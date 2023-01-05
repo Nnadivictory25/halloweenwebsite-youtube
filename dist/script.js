a2f2d50551a6b886e01182d19707219bb6375296
Nnadi Victory <chikamagozi25@gmail.com>
2023-01-05 23:25:49 +0100

    project start

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";var navMenu=document.getElementById("nav-menu"),navToggle=document.getElementById("nav-toggle"),navClose=document.getElementById("nav-close");navToggle&&navToggle.addEventListener("click",(function(){navMenu.classList.add("show-menu")})),navClose&&navClose.addEventListener("click",(function(){navMenu.classList.remove("show-menu")}));
+//# sourceMappingURL=script.js.map
